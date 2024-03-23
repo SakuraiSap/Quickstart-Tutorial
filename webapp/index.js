dc0c210dfb466c49d5a56bdbc1496c1c985f4ed7
SakuraiSap <sakurai.exam@gmail.com>
2024-03-23 11:37:45 +0900

    use MVC to display the app  view.xml control.js

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,10 @@
 sap.ui.define([
-    'sap/m/Button',
-    'sap/m/MessageToast'
-], (Button, MessageToast) => {
-    "use strict";
-    new Button({
-        text: "Ready...",
-        press: function(){
-            MessageToast.show("Hello world")
+    'sap/ui/core/mvc/XMLView'
+], function(XMLView) {
+    'use strict';
+    XMLView.create({viewName: "Quickstart.App"}).then(
+        function(oView){
+            oView.placeAt("content");
         }
-    }).placeAt("content");
+    );
 });
